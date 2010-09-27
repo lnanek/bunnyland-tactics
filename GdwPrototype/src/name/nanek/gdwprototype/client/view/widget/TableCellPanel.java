894aff0748f1f4713fb713ebc1df1c5dd2a71891
lnanek <lnanek@95c4b9ca-7098-8ac2-a198-417fd286d68a>
2010-09-27 04:57:29 +0000

    Simpler client side update processing with less chance of the client side data model and DOM getting out of sync. Much slower, though. Need to rewrite to avoid a slow DOM call.

--- GdwPrototype/src/name/nanek/gdwprototype/client/view/widget/TableCellPanel.java
+++ GdwPrototype/src/name/nanek/gdwprototype/client/view/widget/TableCellPanel.java
@@ -1,6 +1,7 @@
 package name.nanek.gdwprototype.client.view.widget;
 
 import name.nanek.gdwprototype.shared.model.DefaultMarkers;
+import name.nanek.gdwprototype.shared.model.Marker.Layer;
 
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.FlexTable;
@@ -17,6 +18,8 @@ public class TableCellPanel extends AbsolutePanel {
 	private Integer row;
 
 	private Integer column;
+	
+	public GameSquare[] children = new GameSquare[Layer.values().length];
 
 	public TableCellPanel(Widget widget, FlexTable table, int tableRow, int tableCol, Integer gameRow, Integer gameCol) {
 		super();
@@ -29,15 +32,50 @@ public class TableCellPanel extends AbsolutePanel {
 		this.column = gameCol;
 	}
 	
-	
+	@Override
+	public boolean remove(Widget w) {
+		if ( null == w ) {
+			return false;
+		}
+		if ( !( w instanceof GameSquare ) ) {
+			throw new IllegalArgumentException("Only GameSquare widgets may be removed.");
+		}
+		GameSquare square = (GameSquare) w;
+		int layer = square.previousMarker.getLayer().ordinal();
+		children[layer] = null;
+		return super.remove(w);
+	}
 
 	@Override
 	public void add(Widget w) {
-		//All pieces should overlap.
+		if ( null == w ) {
+			return;
+		}
+		if ( !( w instanceof GameSquare ) ) {
+			throw new IllegalArgumentException("Only GameSquare widgets may be added.");
+		}
+		GameSquare square = (GameSquare) w;
+		int layer = square.previousMarker.getLayer().ordinal();
+		
+		GameSquare previousSquare = children[layer];
+		if ( null != previousSquare ) {
+			super.remove(previousSquare);
+		}
+		children[layer] = square;
+		//0, 0 so that all pieces overlap.
 		super.add(w, 0, 0);
 	}
+	
+	
+	
+	//XXX wrap the real panel and only expose own methods? there are other add/remove methods not covered right now
 
-
+	@Override
+	public void clear() {
+		for ( GameSquare child : children ) {
+			remove(child);
+		}
+	}
 
 	public Integer getRow() {
 		return row;
